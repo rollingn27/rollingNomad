5c4430868ad1b0e3608c1817c1c62a09ab4e9eef
rollingn27 <arollingstone27@gmail.com>
2024-04-14 10:33:10 +0900

    페이지 벗어나기, context api 간단 연습 예제

---
+++ src/pages/About.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+
+const About = () => {
+  const [isChange, setIsChange] = useState(false);
+  const confirmMessage =
+    "변경사항이 있습니다. 정말로 이 페이지를 나가시겠습니까?";
+  useEffect(() => {
+    const handleBeforeUnload = (event) => {
+      if (isChange) {
+        event.returnValue = confirmMessage;
+        return confirmMessage;
+      }
+    };
+
+    const handlePopstate = () => {
+      if (isChange) {
+        const navigateFlag = window.confirm(confirmMessage);
+        if (!navigateFlag) {
+          window.history.pushState(null, "", window.location.href);
+        }
+      }
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+    window.addEventListener("popstate", handlePopstate);
+
+    return () => {
+      console.log("destroy");
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+      window.removeEventListener("popstate", handlePopstate);
+    };
+  }, [isChange]);
+
+  const handleInputChange = () => {
+    setIsChange(true);
+  };
+
+  return (
+    <div>
+      <h1>2페이지</h1>
+      <div>페이지 벗어나기 테스트</div>
+      <input onChange={handleInputChange}></input>
+      벗어나기상태: {isChange ? "true" : "false"}
+    </div>
+  );
+};
+
+export default About;
